efc4656981cf671f9b16860621ad0924b42505ec
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-09-04 12:15:46 -0300

    burbujeo

--- main.c
+++ main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define T 10
 //#define T 10 (REEMPLAZA EN T EL NUMERO 10)
+
 
 int main()
 {
-    int vector [5];
+    //int vector [5];
+    //METODO DEL BURBUJEO, QUE HACE? ORDENA DE MAYOR A MENOR Y VICEVERSA
+    int vector [T]={4,2,3,1,5,34,33,8,9,12};
+
+    int i;
+    int j;
+    int aux;
+
+    for(i=0; i<T-1; i++)
+    {
+        for(j=i+1; j<T; j++)
+        {
+            if(vector[i]<vector[j])
+            {
+                aux=vector[i];
+                vector[i]=vector[j];
+                vector[j]=aux;
+            }
+        }
+    }
+
+    for(i=0; i<T ; i++)
+    {
+        printf("%d\n", vector [i]);
+    }
 
     /*vector[2]=9;
 
@@ -14,20 +39,22 @@ int main()
     //PARA QUE EL VECTOR MUESTRE LAs DIRECCIONES DEBEMOS USAR PRINTF("%d", vector)
     //SIEMPRE i=0 y TERMINA CON UN NUMERO MENOR AL LIMITE
 
-    int i;
+
+    /*
     for(i=0; i<5 ; i++)
     {
         printf("Ingrese un numero: ");
         scanf("%d", &vector[i]);
     }
+    */
 //PARA MOSTRAR LOS NUMEROS PARES
     printf("\nTodos los numeros: \n");
-    for(i=0; i<5 ; i++)
+    for(i=0; i<T ; i++)
     {
         printf("%d\n", vector[i]);
     }
     printf("\nTodos los numeros pares: \n");
-    for(i=0; i<5 ; i++)
+    for(i=0; i<T ; i++)
     {
         if(vector[i]%2==0)
         {
