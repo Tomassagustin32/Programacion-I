8f8b9e5f179bad08cb6999597526c66de9498615
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-09-04 11:09:52 -0300

    vectores

--- main.c
+++ main.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void puntero(int*, int );
+//#define T 10 (REEMPLAZA EN T EL NUMERO 10)
 
 int main()
 {
-    int p=10;
-    int q=20;
+    int vector [5];
 
-    puntero(&p,q);
+    /*vector[2]=9;
 
-    printf("Cuanto vale p y q?\n");
-    system("pause");
-    printf("\np=%d\n", p);
-    printf("q=%d", q);
+    printf("%d", vector[2]);
+*/
+    //PARA QUE EL VECTOR MUESTRE LAs DIRECCIONES DEBEMOS USAR PRINTF("%d", vector)
+    //SIEMPRE i=0 y TERMINA CON UN NUMERO MENOR AL LIMITE
+
+    int i;
+    for(i=0; i<5 ; i++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d", &vector[i]);
+    }
+//PARA MOSTRAR LOS NUMEROS PARES
+    printf("\nTodos los numeros: \n");
+    for(i=0; i<5 ; i++)
+    {
+        printf("%d\n", vector[i]);
+    }
+    printf("\nTodos los numeros pares: \n");
+    for(i=0; i<5 ; i++)
+    {
+        if(vector[i]%2==0)
+        {
+            printf("%d\n", vector[i]);
+        }
+
+    }
+//COMO DEFINIR UN VECTOR POR EXTENSION?
+/*  int vector[T] = {1,2,3,4};
+    int i;
+    LO QUE NO PODMOS HACER ES:
+    int vector[T];
+    vector={1,2,3,4}
+    int i; NOoOoO
+    */
     return 0;
-}
-
-void puntero(int* a, int b)
-{
-    *a=b+5;
-    b=9;
 }
