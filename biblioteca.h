5ee46a772f5b301985f0a36d6aac4273dbc23da1
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-09-06 11:37:10 -0300

    CALCULADORA TERMINADA (casi

--- biblioteca.h
+++ biblioteca.h
@@ -1,10 +1,5 @@
-
-/** \brief (resumen)pide un entero y lo retorna
- *
- * \param char[] mensaje para el usuario
- * \return int el numero ingresado
- *
- */
-//FORMA MAS FACIL: SELECCIONAMOS LA FUNCION Y HACEMOS CLICK EN /**, LO COMPLETA AUTOMATICAMENTE
-int pedirEntero(char[]);
-void incrementarValor(int*);
+    int suma(int a, int b);
+ int resta(int a, int b);
+ int multiplicacion(int a, int b);
+ int division(int a, int b);
+ int factorial(int);
--- main.c
+++ main.c
@@ -1,74 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T 10
-//#define T 10 (REEMPLAZA EN T EL NUMERO 10)
-
-
+#include "Biblioteca.h"
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+int division(int a, int b);
 int main()
 {
-    //int vector [5];
-    //METODO DEL BURBUJEO, QUE HACE? ORDENA DE MAYOR A MENOR Y VICEVERSA
-    int vector [T]={4,2,3,1,5,34,33,8,9,12};
+    int opcion;
+    int resultadoSuma;
+    int resultadoResta;
+    int resultadoMultiplicacion;
+    int resultadoDivision;
+    int operadorUno=0;
+    int operadorDos=0;
 
-    int i;
-    int j;
-    int aux;
-
-    for(i=0; i<T-1; i++)
-    {
-        for(j=i+1; j<T; j++)
-        {
-            if(vector[i]<vector[j])
+    do{
+        printf("\nIngrese una opcion: \n");
+        printf("\nOpcion 1:  Ingresar 1er operando (A=x) :");
+        printf("\nOpcion 2:  Ingresar 2do operando (B=y) :");
+        printf("\nOpcion 3:  Calcular todas las operaciones");
+        printf("\nOpcion 4:  Informar resultados");
+        printf("\nOpcion 5:  Salir de la calculadora:");
+        printf("\nOpcion a elegir: ");
+        scanf("%d",&opcion);
+        switch(opcion){
+        case 1:
+            printf("\nIngrese el operador 1: ");
+            scanf("%d", &operadorUno);
+            break;
+        case 2:
+            printf("\nIngrese el operador 2: ");
+            scanf("%d", &operadorDos);
+            break;
+        case 3:
+            resultadoSuma=suma(operadorUno, operadorDos);
+            resultadoResta=resta(operadorUno, operadorDos);
+            resultadoMultiplicacion=multiplicacion(operadorUno, operadorDos);
+            resultadoDivision=division(operadorUno, operadorDos);
+            break;
+        case 4:
+            printf("\nEl resultado de la suma es: %d", resultadoSuma);
+            printf("\nEl resultado de la resta es: %d", resultadoResta);
+            printf("\nEl resultado de la multiplicacion es: %d", resultadoMultiplicacion);
+            if(operadorDos==0)
             {
-                aux=vector[i];
-                vector[i]=vector[j];
-                vector[j]=aux;
+                printf("\nNo se puede dividir por 0");
             }
+            else
+            {
+                resultadoDivision=operadorUno/operadorDos;
+                printf("\nEl resultado de la division es: %d", resultadoDivision);
+            }
+        case 5:
+            printf("\nGracias por utilizar la calculadora de Tomas Martinez");
+            break;
+        default:
+            printf("\nOpcion incorrecta.");
+            printf("\n");
+            break;
         }
-    }
-
-    for(i=0; i<T ; i++)
-    {
-        printf("%d\n", vector [i]);
-    }
-
-    /*vector[2]=9;
-
-    printf("%d", vector[2]);
-*/
-    //PARA QUE EL VECTOR MUESTRE LAs DIRECCIONES DEBEMOS USAR PRINTF("%d", vector)
-    //SIEMPRE i=0 y TERMINA CON UN NUMERO MENOR AL LIMITE
-
-
-    /*
-    for(i=0; i<5 ; i++)
-    {
-        printf("Ingrese un numero: ");
-        scanf("%d", &vector[i]);
-    }
-    */
-//PARA MOSTRAR LOS NUMEROS PARES
-    printf("\nTodos los numeros: \n");
-    for(i=0; i<T ; i++)
-    {
-        printf("%d\n", vector[i]);
-    }
-    printf("\nTodos los numeros pares: \n");
-    for(i=0; i<T ; i++)
-    {
-        if(vector[i]%2==0)
-        {
-            printf("%d\n", vector[i]);
-        }
-
-    }
-//COMO DEFINIR UN VECTOR POR EXTENSION?
-/*  int vector[T] = {1,2,3,4};
-    int i;
-    LO QUE NO PODMOS HACER ES:
-    int vector[T];
-    vector={1,2,3,4}
-    int i; NOoOoO
-    */
+        system("pause");
+        system("cls");
+        printf("El primer numero es: %d", operadorUno);
+        printf("\nEl segundo numero es: %d", operadorDos);
+    }while (opcion != 5);
     return 0;
 }
