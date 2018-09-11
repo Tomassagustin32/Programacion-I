fe6cade8ea7ab3a02f44985045844d71a344d81d
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-09-11 09:45:20 -0300

    ejercicio de la clase 7

--- main.c
+++ main.c
@@ -1,69 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Biblioteca.h"
-int suma(int a, int b);
-int resta(int a, int b);
-int multiplicacion(int a, int b);
-int division(int a, int b);
+#include <string.h>
+#include <ctype.h>
+
 int main()
 {
-    int opcion;
-    int resultadoSuma;
-    int resultadoResta;
-    int resultadoMultiplicacion;
-    int resultadoDivision;
-    int operadorUno=0;
-    int operadorDos=0;
+  /*
+    char cadena1[10];
+    char cadena2[100]="Hola como estas?";
 
-    do{
-        printf("\nIngrese una opcion: \n");
-        printf("\nOpcion 1:  Ingresar 1er operando (A=x) :");
-        printf("\nOpcion 2:  Ingresar 2do operando (B=y) :");
-        printf("\nOpcion 3:  Calcular todas las operaciones");
-        printf("\nOpcion 4:  Informar resultados");
-        printf("\nOpcion 5:  Salir de la calculadora:");
-        printf("\nOpcion a elegir: ");
-        scanf("%d",&opcion);
-        switch(opcion){
-        case 1:
-            printf("\nIngrese el operador 1: ");
-            scanf("%d", &operadorUno);
-            break;
-        case 2:
-            printf("\nIngrese el operador 2: ");
-            scanf("%d", &operadorDos);
-            break;
-        case 3:
-            resultadoSuma=suma(operadorUno, operadorDos);
-            resultadoResta=resta(operadorUno, operadorDos);
-            resultadoMultiplicacion=multiplicacion(operadorUno, operadorDos);
-            resultadoDivision=division(operadorUno, operadorDos);
-            break;
-        case 4:
-            printf("\nEl resultado de la suma es: %d", resultadoSuma);
-            printf("\nEl resultado de la resta es: %d", resultadoResta);
-            printf("\nEl resultado de la multiplicacion es: %d", resultadoMultiplicacion);
-            if(operadorDos==0)
-            {
-                printf("\nNo se puede dividir por 0");
-            }
-            else
-            {
-                resultadoDivision=operadorUno/operadorDos;
-                printf("\nEl resultado de la division es: %d", resultadoDivision);
-            }
-        case 5:
-            printf("\nGracias por utilizar la calculadora de Tomas Martinez");
-            break;
-        default:
-            printf("\nOpcion incorrecta.");
-            printf("\n");
-            break;
+    strncpy(cadena1, cadena2, 4);
+
+    puts(cadena1);
+    funcion atoi=parse int
+    isdigit recibe un caracter y dice si es numero o no
+
+*/
+    char nombre[20];
+    char apellido[20];
+    char nombreCompleto[41];
+    char buffer[1024];
+    int i;
+
+    printf("Ingrese su nombre: ");
+    fflush(stdin);
+    gets(buffer);
+
+    while(strlen(buffer)>19)
+    {
+        printf("Error. Ingrese un nombre valido: ");
+        fflush(stdin);
+        gets(buffer);
+    }
+
+    strcpy(nombre, buffer);
+
+    printf("Ingrese su apellido: ");
+    fflush(stdin);
+    gets(buffer);
+
+    while(strlen(buffer)>19)
+    {
+        printf("Error. Ingrese un apellido valido: ");
+        fflush(stdin);
+        gets(buffer);
+    }
+
+    strcpy(apellido,buffer);
+
+    //O TAMBIEN strcpy(nombreCompleto, apellido);
+
+    strcpy(nombreCompleto, apellido);
+    strcat(nombreCompleto, ", ");
+    strcat(nombreCompleto, nombre);
+
+    strlwr(nombreCompleto);
+
+    nombreCompleto[0]=toupper(nombreCompleto[0]);
+
+    //TAMBIEN PUEDE SER
+
+    for(i=0 ;i<strlen(nombreCompleto); i++)
+    {
+       //if(isspace(palabra[i]));
+       /*
+       {
+       palabra[i+1]=toupper(palabra[i+1]);
+       }
+       */
+        if(nombreCompleto[i]==' ')
+        {
+            nombreCompleto[i+1]=toupper(nombreCompleto[i+1]);
         }
-        system("pause");
-        system("cls");
-        printf("El primer numero es: %d", operadorUno);
-        printf("\nEl segundo numero es: %d", operadorDos);
-    }while (opcion != 5);
+    }
+
+    puts(nombreCompleto);
+
     return 0;
 }
