5fbeeca2e1a5b2b722d3b37c287fe4cef7bd7e34
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2019-03-29 22:43:20 -0300

    funciones y ejercicios

--- main.c
+++ main.c
@@ -3,6 +3,60 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //PIDO UNA PALABRA Y LA MUESTRO
+    /*char palabra[20];
+
+    scanf("%s", palabra);
+    printf("La palabra es: %s", palabra);*/
+//PIDO BASE Y ALTURA Y CALCULO EL AREA
+    /*int base;
+    int altura;
+    int area;
+
+    printf("Ingrese base: ", &base);
+    scanf("%d", &base);
+    printf("Ingrese altura: ", &altura);
+    scanf("%d", &altura);
+    area=(base*altura)/2;
+    printf("El area es: %d", area);*/
+//PIDO 5 NUMEROS Y MUESTRO EL MAXIMO, EL MINIMO Y EL PROMEDIO
+    /*int maximo;
+    int minimo;
+    int promedio;
+    int acumulador;
+    int auxInt;
+    int i;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &auxInt);
+
+    minimo=auxInt;
+    maximo=auxInt;
+    acumulador=auxInt;
+
+    for(i=0; i<4; i++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d", &auxInt);
+        acumulador=acumulador+auxInt;
+
+        if(auxInt>maximo)
+        {
+            maximo=auxInt;
+        }
+        if(auxInt<minimo)
+        {
+            minimo=auxInt;
+        }
+
+    }
+//LOS MUESTRO
+    printf("El maximo es: %d\n", maximo);
+    printf("El minimo es: %d\n", minimo);
+    printf("El promedio es: %d", acumulador/5);
+
+*/
+
+
     return 0;
 }
